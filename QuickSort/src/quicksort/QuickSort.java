0412ac7b09073c047a84498c7ddd67758a917735
Bárbara Santana <55034150+babqs@users.noreply.github.com>
2020-08-27 22:49:15 -0300

    Add files via upload

--- QuickSort/src/quicksort/QuickSort.java
+++ QuickSort/src/quicksort/QuickSort.java
@@ -6,8 +6,8 @@
 package quicksort;
 
 
-import java.util.ArrayList;
-import java.util.List;
+//import java.util.ArrayList;
+//import java.util.List;
 import java.util.concurrent.RecursiveTask;
 
 /**
