c590a72d1d237453a8ebc3d08fa4944da6ad0ecb
Bárbara Santana <55034150+babqs@users.noreply.github.com>
2020-09-18 14:09:49 -0300

    Add files via upload

--- FrequenciaPalavra/src/frequenciapalavra/FrequenciaPalavra.java
+++ FrequenciaPalavra/src/frequenciapalavra/FrequenciaPalavra.java
@@ -20,10 +20,39 @@ public class FrequenciaPalavra extends RecursiveTask {
     String maiusculo, maiusculoSW;
     String linha, linhaSW;
     Map<String, Integer> contar = new TreeMap<>();
+    ArrayList<String> lista;
+    int inicio, fim;
 
-    public FrequenciaPalavra() throws FileNotFoundException {
+    public FrequenciaPalavra(ArrayList<String> lista, int inicio, int fim) throws FileNotFoundException {
         arquivo = new FileReader("texto.txt");
         stopwords = new FileReader("stopwords.txt");
+        lista = new ArrayList<>();
+        this.inicio = inicio;
+        this.fim = fim;
+    }
+
+    public FrequenciaPalavra(ArrayList<String> lista) throws FileNotFoundException {
+        this(lista, 0, lista.size());
+    }
+
+    public void leArquivo() {
+        try (BufferedReader buff = new BufferedReader(arquivo)) {
+            BufferedReader buffSW = new BufferedReader(stopwords);
+            //lê a primeira linha do arquivo
+            linha = buff.readLine();
+            linhaSW = buffSW.readLine();
+            while (linha != null) {
+                modificaArquivo();
+                //lê as próximas linhas do arquivo
+                linha = buff.readLine();
+            }
+            buff.close();
+
+            imprimeFrequencia();
+
+        } catch (IOException ex) {
+            Logger.getLogger(FrequenciaPalavra.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void modificaArquivo() {
@@ -54,50 +83,44 @@ public class FrequenciaPalavra extends RecursiveTask {
                 lista.add(p);
             }
         }
-        calcularFrequencia(lista);
+        calcularFrequencia(lista, 0, lista.size());
     }
 
-    public void calcularFrequencia(ArrayList<String> lista) {
+    public Integer calcularFrequencia(ArrayList<String> lista, int inicio, int fim) {
         //conta a frequencia de cada palavra
+        String p;
+        Integer frequencia = 0;
         for (int i = 0; i < lista.size(); i++) {
-            String p = lista.get(i);
-            Integer frequencia = contar.get(p);
+            p = lista.get(i);
+            frequencia = contar.get(p);
             if (frequencia != null) {
                 contar.put(p, frequencia + 1);
             } else {
                 contar.put(p, 1);
             }
         }
+        return 0;
     }
 
     public void imprimeFrequencia() {
-        //imprime uma nova lista sem as stopwords
+        //imprime a frequencia de cada palavra e em ordem alfabética
         contar.entrySet().forEach((f) -> {
             System.out.println(f.getKey() + "\t Frequência: " + f.getValue());
         });
     }
 
     @Override
-    protected Integer compute(){
-        try (BufferedReader buff = new BufferedReader(arquivo)) {
-            BufferedReader buffSW = new BufferedReader(stopwords);
-            //lê a primeira linha do arquivo
-            linha = buff.readLine();
-            linhaSW = buffSW.readLine();
-            while (linha != null) {
-                modificaArquivo();
-                //lê as próximas linhas do arquivo
-                linha = buff.readLine();
-            }    
-            buff.close();
-
-            imprimeFrequencia();
-            
-        } catch (IOException ex) {
-            Logger.getLogger(FrequenciaPalavra.class.getName()).log(Level.SEVERE, null, ex);
+    protected Integer compute() {
+        leArquivo();
+        int m;
+        try {
+            if (inicio < fim) {
+                m = calcularFrequencia(lista, 0, lista.size());
+                invokeAll(new FrequenciaPalavra(lista, 0, m - 1).fork(), new FrequenciaPalavra(lista, m + 1, lista.size()).fork());
+            }
+        } catch (FileNotFoundException e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
         }
-    return 0;
-}
-    
-    
+        return 0;
+    }
 }
--- FrequenciaPalavra/src/frequenciapalavra/Principal.java
+++ FrequenciaPalavra/src/frequenciapalavra/Principal.java
@@ -2,13 +2,18 @@ package frequenciapalavra;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ForkJoinPool;
 
 public class Principal {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
-         
-        FrequenciaPalavra f = new FrequenciaPalavra();
         
-        f.compute();
+        ArrayList<String> tarefa = new ArrayList<>();
+        FrequenciaPalavra f = new FrequenciaPalavra(tarefa);
+        
+       ForkJoinPool pool = new ForkJoinPool();
+       pool.invoke(f);
+     
     }
 }
